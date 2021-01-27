b4f75619a949e691a7475770902d88ad75dcd6bc
Apu Fouzder <apufouzdar@gmail.com>
2021-01-27 14:22:05 +0800

    Complete Shopping Card

--- script.js
+++ script.js
@@ -1,43 +1,107 @@
-// First Item amount
-// 1
+// First Item Price Calculations selected id.
 var deCreaseItem1 = document.getElementById("deCreaseItem1");
 var inputItem1 = document.getElementById("inputItem1");
-
-deCreaseItem1.addEventListener("click", function () {
-    var firstParser1 = parseInt(inputItem1.value);
-    firstParser1--;
-    inputItem1.value = firstParser1;
-    priceItem1.innerText = 1200 * parseInt(inputItem1.value);
-})
-// 2
 var inCreaseItem1 = document.getElementById("inCreaseItem1");
 var priceItem1 = document.getElementById("priceItem1");
 
-inCreaseItem1.addEventListener("click", function () {
-    var firstParser2 = parseInt(inputItem1.value);
-    firstParser2++;
-    inputItem1.value = firstParser2;
-    priceItem1.innerText = 1200*parseInt(inputItem1.value);
+
+// First item deCreaseItem1.addEventListener..
+deCreaseItem1.addEventListener("click", function () {
+    decreaserItemFun(1219, inputItem1, priceItem1);
 })
 
-// Second Item amount
+// First item inCreaseItem1.addEventListener..
+inCreaseItem1.addEventListener("click", function () {
+    increaserItemFun(1219, inputItem1, priceItem1)
+})
+
+
+// Second Item Price Calculations selected id.
 var deCreaseItem2 = document.getElementById("deCreaseItem2");
 var inputItem2 = document.getElementById("inputItem2");
-
-deCreaseItem2.addEventListener("click", function () {
-    var secondParser1 = parseInt(inputItem2.value);
-    secondParser1--;
-    inputItem2.value = secondParser1;
-    priceItem2.innerText = 59 * parseInt(inputItem2.value);
-})
-// 2
 var inCreaseItem2 = document.getElementById("inCreaseItem2");
 var priceItem2 = document.getElementById("priceItem2");
 
-inCreaseItem2.addEventListener("click", function () {
-    var secondParser2 = parseInt(inputItem2.value);
-    secondParser2++;
-    inputItem2.value = secondParser2;
-    priceItem2.innerText = 59 * parseInt(inputItem2.value);
+
+// Second item deCreaseItem2.addEventListener..
+deCreaseItem2.addEventListener("click", function () {
+    decreaserItemFun(59, inputItem2, priceItem2);
 })
+
+// Second item inCreaseItem2.addEventListener..
+inCreaseItem2.addEventListener("click", function () {
+    increaserItemFun(59, inputItem2, priceItem2);
+})
+
+
+// all DecreaserItem Function
+function decreaserItemFun(price, inputItem, priceItem) {
+    // Only item decrease
+    var firstParser1 = parseInt(inputItem.value);
+    firstParser1--;
+    inputItem.value = firstParser1;
+    priceItem.innerText = price * parseInt(inputItem.value);
+    // Total cost calculation
+    subTotalCost.innerText = parseInt(priceItem1.innerText) + parseInt(priceItem2.innerText);
+    taxCost.innerText = parseInt(subTotalCost.innerText / 100);
+    totalCost.innerText = parseInt(subTotalCost.innerText) + (taxCost.innerText);
+}
+
+// all IncreaserItem Function
+function increaserItemFun(price, inputItem, priceItem) {
+    // Only item increase
+    var firstParser1 = parseInt(inputItem.value);
+    firstParser1++;
+    inputItem.value = firstParser1;
+    priceItem.innerText = price * parseInt(inputItem.value);
+    // Total cost calculation
+    subTotalCost.innerText = parseInt(priceItem1.innerText) + parseInt(priceItem2.innerText);
+    taxCost.innerText = parseInt(subTotalCost.innerText / 100);
+    totalCost.innerText = parseInt(subTotalCost.innerText) + (taxCost.innerText);
+}
+
+// subTotalCost, taxCost, totalCost, Calculation selected Id.
+var subTotalCost = document.getElementById("subTotalCost");
+var taxCost = document.getElementById("taxCost");
+var totalCost = document.getElementById("totalCost");
+
+
+// Cancel a item selected id.
+const firstItem = document.getElementById("firstItem");
+const cancelItem1 = document.getElementById("cancelItem1");
+const secondItem = document.getElementById("secondItem");
+const cancelItem2 = document.getElementById("cancelItem2");
+
+// First item cancel addEventListener..
+cancelItem1.addEventListener("click", function(){
+    firstItem.style.display = "none";
+    priceItem1.innerText = 0;
+    // Total cost calculation
+    subTotalCost.innerText = parseInt(priceItem1.innerText) + parseInt(priceItem2.innerText);
+    taxCost.innerText = parseInt(subTotalCost.innerText / 100);
+    totalCost.innerText = parseInt(subTotalCost.innerText) + (taxCost.innerText);
+})
+
+// Second item cancel addEventListener..
+cancelItem2.addEventListener("click", function(){
+    secondItem.style.display = "none";
+    priceItem2.innerText = 0;
+    // Total cost calculation
+    subTotalCost.innerText = parseInt(priceItem1.innerText) + parseInt(priceItem2.innerText);
+    taxCost.innerText = parseInt(subTotalCost.innerText / 100);
+    totalCost.innerText = parseInt(subTotalCost.innerText) + (taxCost.innerText);
+})
+
+
+// Check Out  related selected id.
+const checkOutButton = document.getElementById("checkOutButton");
+const section = document.getElementById("section");
+const conformOrder = document.getElementById("conformOrder");
+
+checkOutButton.addEventListener("click", function(){
+    section.style.display = "none";
+    conformOrder.style.display = "block";
+})
+
+
 
